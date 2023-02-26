4a1a486b577b99aa96cd29ba49eee24d09afb5d7
nguyen viet thinh <112790614+nvtmeta@users.noreply.github.com>
2023-02-26 15:50:54 +0700

    bannerfixslide

--- frontend/vite-project/src/components/Banner.jsx
+++ frontend/vite-project/src/components/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from '../style';
 import bannerImg from '../assets/banner.png';
 import bannerImg2 from '../assets/banner2.jpg';
-import bannerImg3 from '../assets/images-_1_.svg';
 import { Slide } from 'react-slideshow-image';
+import bannerImg3 from '../assets/images-_1_.svg';
 
 const Banner = () => {
   const imgCollection = [
@@ -26,55 +26,58 @@ const Banner = () => {
   return (
     <div
       className={`${styles.boxWidth} 
-      flex justify-between items-center   
+      flex flex-col  
   `}
     >
-      <Slide>
-        {imgCollection.map((item) => (
-          <div
-            key={item.id}
-            className={`w-[90%] 
+      <div>
+        <Slide>
+          {imgCollection.map((item) => (
+            <div
+              key={item.id}
+              className={`w-[90%] 
               mx-auto smooth-transition flex  rounded-3xl 
               ${item.id === 3 ? 'banner-gradient' : ''}
               `}
-          >
-            {/* banner content img 3 */}
-            {item.id === 3 ? (
-              <div
-                className={`${styles.boxWidth} h-[300px]
+            >
+              {/* banner content img 3 */}
+              {item.id === 3 ? (
+                <div
+                  className={`${styles.boxWidth} h-[500px]
               flex  items-center 
               
               `}
-              >
-                <div
-                  className={` text-white font-poppins
+                >
+                  <div
+                    className={` text-white font-poppins
                    font-bold
                    p-6 w-full `}
-                >
-                  <h1 className={`${styles.heading2}`}>
-                    Học viện cầu lông <span className="text-gradient">CKH</span>
-                  </h1>
-                  <p className={`${styles.paragraph} font-sans mt-4 `}>
-                    Tuyển sinh lớp cầu lông cơ bản và nâng cao dành cho trẻ em
-                    và người lớn
-                  </p>
+                  >
+                    <h1 className={`${styles.heading2}`}>
+                      Học viện cầu lông{' '}
+                      <span className="text-gradient">CKH</span>
+                    </h1>
+                    <p className={`${styles.paragraph} font-sans mt-4 `}>
+                      Tuyển sinh lớp cầu lông cơ bản và nâng cao dành cho trẻ em
+                      và người lớn
+                    </p>
+                  </div>
+                  {/* imgbanner */}
+                  <div>
+                    <img src={bannerImg3} />
+                  </div>
                 </div>
-                {/* imgbanner */}
-                <div>
-                  <img src={bannerImg3} />
-                </div>
-              </div>
-            ) : (
-              <img
-                className={` object-contain  
-                rounded-3xl w-full auto`}
-                src={item.imgFile}
-                alt={item.imgName}
-              />
-            )}
-          </div>
-        ))}
-      </Slide>
+              ) : (
+                <img
+                  className={` object-cover  
+                rounded-3xl w-full auto h-[500px] `}
+                  src={item.imgFile}
+                  alt={item.imgName}
+                />
+              )}
+            </div>
+          ))}
+        </Slide>
+      </div>
     </div>
   );
 };
